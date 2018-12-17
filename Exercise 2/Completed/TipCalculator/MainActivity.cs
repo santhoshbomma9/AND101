c44c8195a1d6a00b5ea7a6fd6fc5e475583b8195
Santhosh Bomma <santhosh_bomma@dialog.com.au>
2018-12-17 11:41:59 +1100

    implemented calculate functionality

--- Exercise 2/Completed/TipCalculator/MainActivity.cs
+++ Exercise 2/Completed/TipCalculator/MainActivity.cs
@@ -7,10 +7,37 @@ namespace TipCalculator
 	[Activity(Label = "TipCalculator", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
-		protected override void OnCreate(Bundle savedInstanceState)
+
+        EditText inputBill;
+        Button calculateButton;
+        TextView outputTip;
+        TextView outputTotal;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
 			SetContentView(Resource.Layout.Main);
-		}
-	}
+
+          
+            inputBill = FindViewById<EditText>(Resource.Id.inputBill);
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+
+            calculateButton.Click += CalculateButton_Click;
+        }
+
+        private void CalculateButton_Click(object sender, System.EventArgs e)
+        {
+            string text = inputBill.Text;
+            var bill = double.Parse(text);
+
+            var tip = bill * 0.15;
+            var total = bill + tip;
+
+            outputTip.Text = tip.ToString();
+            outputTotal.Text = total.ToString();
+        }
+    }
 }
