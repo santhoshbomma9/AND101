5f2ce8d68834840c5dc07b888a83fc803afaf8b8
Santhosh Bomma <santhosh_bomma@dialog.com.au>
2018-12-17 11:08:56 +1100

    made changs to reflect tip calcualtor app

--- Exercise 1/Completed/TipCalculator/MainActivity.cs
+++ Exercise 1/Completed/TipCalculator/MainActivity.cs
@@ -12,18 +12,7 @@ namespace TipCalculator
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
-
-			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
-
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
-			
-			button.Click += delegate
-			{
-				button.Text = string.Format("{0} clicks!", count++);
-			};
 		}
 	}
 }
